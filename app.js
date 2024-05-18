04a4abeef474e444a6d34740ef61880464a61a08
Mursch Arthur <arthur.mursch@gmail.com>
2024-05-18 19:30:07 +0200

    ajout authentification

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const userRoutes = require("./routes/user");
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:hRQhn8vk4aUKtw2weDTuFBwxFyb5FY@cluster0.kdlqebf.mongodb.net/MonVieuxGrimoire"
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
 
 app.use((req, res, next) => {
-  console.log("Requête reçue !");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+app.use(express.json());
 
-app.use((req, res, next) => {
-  res.json({ message: "Votre requête a bien été reçue !" });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("Réponse envoyée avec succès !");
-});
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
