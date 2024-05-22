8196aa1c1871a4441092fc142a74239ba92399fa
Mursch Arthur <arthur.mursch@gmail.com>
2024-05-22 12:10:07 +0200

    modif sharp

--- middleware/sharp-config.js
+++ middleware/sharp-config.js
@@ -1,32 +1,45 @@
 const sharp = require("sharp");
 const fs = require("fs");
+const path = require("path");
 
 const optimizeImg = async (req, res, next) => {
   if (!req.file) return next();
   const originalImagePath = req.file.path;
-  const optimizedImageName = `optimized_${req.file.filename}`;
-  const optimizedImagePath = `images/${optimizedImageName}`;
-  try {
-    await sharp(originalImagePath)
-      .webp({ quality: 80 })
-      .resize(400)
-      .toFile(optimizedImagePath)
-      .then(() => {
-        req.file.path = optimizedImagePath;
-        req.file.filename = optimizedImageName;
-        // Supprimer l'image originale
-        fs.unlink(originalImagePath, (err) => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-          console.log("Original image deleted successfully");
-        });
+  const filename = originalImagePath.split("images\\")[1];
+  console.log(filename);
+  const optimizedImageName = `${path.basename(
+    originalImagePath,
+    path.extname(originalImagePath)
+  )}.webp`;
+  const optimizedImagePath = path.join(
+    path.dirname(originalImagePath),
+    optimizedImageName
+  );
+
+  sharp(originalImagePath)
+    .webp({ quality: 80 })
+    .resize(400)
+    .toFile(optimizedImagePath, (error, info) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      console.log(info);
+
+      fs.unlink(`images/${filename}`, (unlinkErr) => {
+        if (unlinkErr) {
+          console.error(unlinkErr);
+        } else {
+          console.log("\nDeleted Image: ", originalImagePath);
+        }
       });
-    next();
-  } catch (err) {
-    console.error(err);
-  }
+      req.file.path = optimizedImagePath;
+      req.file.filename = optimizedImageName;
+      console.log(req.file.path);
+      console.log(originalImagePath); // undefined
+      next();
+    });
 };
 
 module.exports = optimizeImg;
